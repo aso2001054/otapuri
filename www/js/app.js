d4fd09b31f8c6066b1d29a9d0dd3ac9e7e1833dc
aso2001054 <2001054@s.asojuku.ac.jp>
2021-07-20 16:42:30 +0900

    Project's Initial Commit

---
+++ www/js/app.js
@@ -0,0 +1,5 @@
+// This is a JavaScript file
+const applicationKey  = 'b7bd379fb1087140b86a0018fee39e3e615bffd0548e92a23d7505cd9bc242ee';
+const clientKey       = '447bcae2e21050925ad27cd73e28f48859df96e1a3c2de8fb6fe35fca2abdb44';
+const ncmb = new NCMB(applicationKey, clientKey);
+
